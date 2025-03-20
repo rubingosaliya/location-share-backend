dfe16470c9297128bb0639ee68cdc4f5436f2b99
rubingosaliya <rubingosaliya@gmail.com>
2025-03-19 21:10:32 -0700

    Add name field to location storage and return it in GET response

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-// Allow JSON data and cross-origin requests
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -10,19 +9,16 @@ app.use((req, res, next) => {
   next();
 });
 
-// Store locations in memory (temporary for testing)
 let locations = {};
 
-// POST endpoint to receive a location with name
 app.post('/locations/:sessionId', (req, res) => {
   const { sessionId } = req.params;
-  const { lat, lng, name } = req.body; // Add name here
+  const { lat, lng, name } = req.body;
   if (!locations[sessionId]) {
     locations[sessionId] = [];
   }
-  locations[sessionId].push({ lat, lng, name }); // Store name
-  console.log(`Added to ${sessionId}:`, { lat, lng, name }); // Debug log
-  // Clear after 60 minutes (3600000 milliseconds)
+  locations[sessionId].push({ lat, lng, name });
+  console.log(`Added to ${sessionId}:`, { lat, lng, name });
   setTimeout(() => {
     delete locations[sessionId];
     console.log(`Cleared session ${sessionId}`);
@@ -30,12 +26,10 @@ app.post('/locations/:sessionId', (req, res) => {
   res.send('Location added');
 });
 
-// GET endpoint to send all locations for a session
 app.get('/locations/:sessionId', (req, res) => {
   const { sessionId } = req.params;
-  console.log(`Sending for ${sessionId}:`, locations[sessionId] || []); // Debug log
+  console.log(`Sending for ${sessionId}:`, locations[sessionId] || []);
   res.json({ locations: locations[sessionId] || [] });
 });
 
-// Export for Vercel (instead of app.listen)
 module.exports = app;
