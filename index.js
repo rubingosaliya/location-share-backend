6a5c6067850a24f356d148aaa4ac2c847c3f246f
rubingosaliya <rubingosaliya@gmail.com>
2025-03-23 12:14:11 -0700

    Direct handler test

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Root works', name: 'Rubin' });
-});
-
-app.get('/test', (req, res) => {
-  res.json({ message: 'Test works', name: 'Rubin' });
-});
-
-// Explicit export for Vercel
-module.exports = {
-  path: '/api',
-  handler: app
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Direct handler works', name: 'Rubin' });
 };
