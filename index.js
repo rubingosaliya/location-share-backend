19d2e2603edaaba69a51ac3592c6aca4e466bdbf
rubingosaliya <rubingosaliya@gmail.com>
2025-03-23 12:17:41 -0700

    Ultra-minimal test

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Minimal test for Vercel
 module.exports = (req, res) => {
-  res.status(200).json({ message: 'Direct handler works', name: 'Rubin' });
+  res.status(200).json({ message: 'Minimal test', name: 'Rubin' });
 };
