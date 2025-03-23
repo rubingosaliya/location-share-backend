d6ea34ecc9a17f056750f85cc3fe18bd6741b3f4
rubingosaliya <rubingosaliya@gmail.com>
2025-03-23 11:57:37 -0700

    Minimal test endpoint

--- index.js
+++ index.js
@@ -2,42 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
-let locations = {};
-
-app.post('/locations/:sessionId', (req, res) => {
-  const { sessionId } = req.params;
-  console.log('Raw body:', req.body);
-  const { lat, lng, name } = req.body;
-  if (!locations[sessionId]) {
-    locations[sessionId] = [];
-  }
-  const loc = { lat, lng, name: name || 'ForcedName', debug: 'POST worked' };
-  locations[sessionId].push(loc);
-  console.log('Stored:', locations[sessionId]);
-  res.send('Location added');
-});
-
-app.get('/locations/:sessionId', (req, res) => {
-  const { sessionId } = req.params;
-  console.log('Returning:', locations[sessionId] || []);
-  res.json({ locations: locations[sessionId] || [] });
-});
-
-// Debug endpoint
-app.get('/debug', (req, res) => {
-  res.json({
-    message: 'Debug route active',
-    code: 'Latest with name handling',
-    locations: locations
-  });
+app.get('/test', (req, res) => {
+  res.json({ message: 'Test works', name: 'Rubin' });
 });
 
 module.exports = app;
-// Clear cache test
