22e9dca9189a76d519213ecc277810bef40e9a7d
rubingosaliya <rubingosaliya@gmail.com>
2025-03-23 12:03:47 -0700

    Add root endpoint for debug

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Root works', name: 'Rubin' });
+});
+
 app.get('/test', (req, res) => {
   res.json({ message: 'Test works', name: 'Rubin' });
 });
